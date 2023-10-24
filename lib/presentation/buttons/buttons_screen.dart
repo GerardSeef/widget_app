c8d53cfaaac5f72144785b39fbfdfd43e870d388
GerardSeef <gerardseef4@gmail.com>
2023-10-24 18:38:52 -0300

    button personalizado

--- lib/presentation/buttons/buttons_screen.dart
+++ lib/presentation/buttons/buttons_screen.dart
@@ -78,7 +78,7 @@ class _ButtonsView extends StatelessWidget {
                 label: const Text('Text Icon'),
               ),
 
-              //TODO: custom button
+              const CustomButton(),
 
               IconButton(
                 onPressed: () {},
@@ -91,6 +91,32 @@ class _ButtonsView extends StatelessWidget {
                 iconColor: const MaterialStatePropertyAll( Colors.white)),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {
+            
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: const Text('Hola mundo', style: TextStyle(color: Colors.white),),
           ),
         ),
       ),
